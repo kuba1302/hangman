cbf7127382d7bc68d7aa4644af7c036f6fef02cd
kuba1302 <kuba1302@gmail.com>
2021-01-05 14:42:19 +0100

    data

--- spiders/price_spyder.py
+++ spiders/price_spyder.py
@@ -1,3 +1,17 @@
 import scrapy
+import pandas as pd
 
-"check git"
+
+# Get clear link
+data = pd.read_csv(r'C:/Users/Admin/Desktop/UW/PYTHON/Projekt/price_comparison/price_comparison/output.csv')
+data['links'] = data['links'].apply(lambda x: x.split("'")[1])
+
+
+
+
+class PriceSpider(scrapy.Spider):
+    name = "price"
+    start_urls = [
+        data['links'][1]
+
+    ]
