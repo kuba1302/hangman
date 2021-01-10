eca243916decdef606b8f1b80f32b7be8ca436f8
kuba1302 <kuba1302@gmail.com>
2021-01-10 01:25:34 +0100

    it works

--- spiders/main.py
+++ spiders/main.py
@@ -1,6 +1,6 @@
 import scrapy
 from scrapy.crawler import CrawlerProcess
-
+import pandas as pd
 from price_comparison.price_comparison.spiders.price_spyder import *
 
 
@@ -12,17 +12,31 @@ process = CrawlerProcess(settings={
     },
 })
 
-spiders = [PriceSpider,PriceSpider2,PriceSpider3,PriceSpider4,PriceSpider5,PriceSpider6,PriceSpider7,PriceSpider8,PriceSpider9,PriceSpider10]
+spiders = [PriceSpider ,PriceSpider2 ,PriceSpider3 ,PriceSpider4,
+           PriceSpider5 ,PriceSpider6 ,PriceSpider7, PriceSpider8,
+           PriceSpider9, PriceSpider10]
 
 if __name__ == "__main__":
-    for spider in spiders:
-        process.crawl(spider)
-    process.start()
-    df = pd.read_csv(r'C:\Users\Admin\Desktop\UW\PYTHON\Projekt\price_comparison\price_comparison\spiders\vodka.csv')
-    df.dropna()
-    df = df[~df['price'].isnull()]
-    df.drop(df.loc[df['price'] == 'price'].index, inplace=True)
-    df['price'] = df['price'].apply(lambda x: x.replace(",", "."))
-    df['price'] = df['price'].str.extract(r'(\d+.\d+)').astype('float')
-    # df.loc[df['product'].str.contains("Beluga", na=False)]
+    # for spider in spiders:
+    #     process.crawl(spider)
+    # process.start()
+    while True:
+        df = pd.read_csv(r'C:\Users\Admin\Desktop\UW\PYTHON\Projekt\price_comparison\price_comparison\spiders\vodka.csv')
+        df.dropna()
+        df = df[~df['price'].isnull()]
+        df.drop(df.loc[df['price'] == 'price'].index, inplace=True)
+        df['price'] = df['price'].apply(lambda x: x.replace(",", "."))
+        df['price'] = df['price'].str.extract(r'(\d+.\d+)').astype('float')
+        checked_vodka = input("What vodka do you want to check?\n")
+        check_df = df.loc[df['product'].str.contains("{}".format(checked_vodka), na=False)]
+        minimum = check_df.loc[check_df['price'] == check_df['price'].min()]
+        print(minimum)
+        if_continue = input("Press Y to chose another vodka, press N to leave \n")
+        if if_continue == "Y":
+            pass
+        elif if_continue == "N":
+            break
+
+
+    # df.loc[df['product'].str.containssssssdaa", na=False)]
 
